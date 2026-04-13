73e5cda189387854c314f2f2436dfd23eae678c4
Rachita Batra <104453806+RachitaB@users.noreply.github.com>
2026-04-13 12:05:13 +0530

    Update compound_interest.py

--- Python/Easy/Compound Interest/compound_interest.py
+++ Python/Easy/Compound Interest/compound_interest.py
@@ -6,3 +6,12 @@ def compound_interest(principal, rate, contribution, years):
     principal = interest + principal + contribution
   
   return round(principal,2)
+
+# Solution 2
+def compound_interest(principal, rate, contribution, years):
+  
+  final_amount = 0
+  r = rate/100
+  
+  final_amount = principal * ((1+r)**years) + contribution * (((1+r)**years -1) /r)
+  return round(final_amount,2)
